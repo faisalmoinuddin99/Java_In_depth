13da21ae7b707eadd8018b58c02f8d282be499bd
faisal <faisal25march99@gmail.com>
2021-03-22 15:18:52 +0530

    strictfp keword in java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,29 +1,39 @@
 package com.company;
 
+/*
+strictfp keyword in java :
+
+- strictfp is a keyword in java used for restricting floating-point calculations and ensuring same result on every platform while performing operations in the floating-point variable.
+Floating point calculations are platform dependent i.e. different output(floating-point values) is achieved when a class file is run on different platforms(16/32/64 bit processors).
+To solve this types of issue, strictfp keyword was introduced in JDK 1.2 version by following IEEE 754 standards for floating-point calculations.
+
+- When a class or an interface is declared with strictfp modifier, then all methods declared in the class/interface, and all nested types declared in the class, are implicitly strictfp.
+
+- strictfp cannot be used with abstract methods. However, it can be used with abstract classes/interfaces.
+- Since methods of an interface are implicitly abstract, strictfp cannot be used with any method inside an interface.
+ */
+
+strictfp interface Sum {
+    double sum(float a, float b);
+}
+
+class Calculator implements Sum {
+
+
+    @Override
+    public double sum(float x, float y) {
+        return x + y;
+    }
+}
+
 public class Main {
 
-    /*
-    which of the following valid java identifier
-     */
-    public static void main(String[] args) {
-// write your code here
-        int _$_ = 10 ; // valid
-        int ca$h = 1000 ; // valid
-        String java2share = "yes" ; // valid
-//        int all@hands  --> invalid
-//        int 123abc  -- > invalid
-        int $hello = 6 ; // valid
-        int Int = 60 ; // valid ( not a good practice )
-        int Integer = 80; // valid ( not a good practice )
-//        int int --> invalid
-        int total234 = 90 ; // valid
 
-        System.out.println(_$_); // 10
-        System.out.println(ca$h); // 1000
-        System.out.println(java2share); // yes
-        System.out.println($hello); // 6
-        System.out.println(Int); // 60
-        System.out.println(Integer); // 80
-        System.out.println(total234); // 90
+    public static void main(String[] args) {
+
+    Calculator ca = new Calculator();
+        System.out.println(ca.sum(86.7f, 3.14f)); // 89.83999633789062
+
+
     }
 }
